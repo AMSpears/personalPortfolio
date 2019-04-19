b32de691c7266d3d1e417c99a45c5f973ca6e95e
Angie Spears <angie.spears809@gmail.com>
2019-04-19 13:53:06 -0400

    organize css

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { Link } from "gatsby"
 import "gatsby-plugin-sass"
 import Resume from "../resume/AngieSpears_Resume.pdf"
 
@@ -9,10 +10,10 @@ const Header = ({ siteTitle, home, aboutMe, resume }) => (
   <header>
       <nav>
         <ul>
-          <li><a  href='/' id= 'logo'> {siteTitle}</a></li>
+          <li><Link to='/' id= 'logo'> {siteTitle}</Link></li>
           <div className= 'linksContainer'>
-            <li><a href= '/' className= 'links'>{home}</a></li>
-            <li><a href= '#aboutMe' className= 'links'>{aboutMe}</a></li>
+            <li><Link to= '/' className= 'links'>{home}</Link></li>
+            <li><Link to= '#aboutMe' className= 'links'>{aboutMe}</Link></li>
             <li><a href= {Resume} target= "blank"  className= 'links' id= 'resume'>{resume}</a></li>
           </div>
         </ul>
