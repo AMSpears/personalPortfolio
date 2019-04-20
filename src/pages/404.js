71596549b8cfb04d2a9689416efbaac765c66895
Angie Spears <angie.spears809@gmail.com>
2019-04-20 14:58:58 -0400

    add new assets and update css

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,15 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import "../components/404.scss"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className = "messageContainer">
+      <h1>404 | PAGE NOT FOUND</h1>
+      <p>Looks like you type the wrong address. You can use the navegation above to go to the correct page.</p>
+    </div>
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,21 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Samsung from "../images/sg_logo.png"
-import Verizon from "../images/vzlogo_lg.png"
-import SFTProject from "../images/projects/SFT_Project.jpg"
-import FRSProject from "../images/projects/FRS_Project.jpg"
-import AboveAllSecurity from "../images/projects/above-all-security.jpg"
-import ProvideServices from "../images/projects/provide-services.jpg"
-import WeatherApp from "../images/projects/weather-app.jpg"
-import FoodSearches from "../images/projects/food-searches.jpg"
-import TriviaGame from "../images/projects/trivia-game.jpg"
-import ShoppingList from "../images/projects/shopping-list.jpg"
-import HomeFinder from "../images/projects/home-finder.jpg"
-
-
-
-import {
-  FaGithub, FaCodepen, FaLinkedinIn
-} from 'react-icons/fa';
+// brands
+import Samsung from "../images/logos/samsung.png"
+import Verizon from "../images/logos/verizon.png"
+//projects
+import SFTProject from "../images/projects/SFT_Project.png"
+import FRSProject from "../images/projects/FRS_Project.png"
+import AboveAllSecurity from "../images/projects/above-all-security.png"
+// import ProvideServices from "../images/projects/provide-services.png"
+import WeatherApp from "../images/projects/weather-app.png"
+import FoodSearches from "../images/projects/food-searches.png"
+import TriviaGame from "../images/projects/trivia-game.png"
+import ShoppingList from "../images/projects/shopping-list.png"
+import HomeFinder from "../images/projects/home-finder.png"
+//icons
+import { FaGithub, FaCodepen, FaLinkedinIn } from 'react-icons/fa';
 
 import "../components/index.scss"
 
@@ -39,13 +37,16 @@ const IndexPage = () => (
         <h1>Brands I've worked with</h1>
         <hr/>
         <div className = "brandLogos">
-          <img id ="samsung" src= {Samsung} alt= "samsung"/>
-          <img id = "verizon" src= {Verizon} alt= "verizon"/>
+          <div><img id ="samsung" src= {Samsung} alt= "samsung"/></div>
+          <div><img id = "verizon" src= {Verizon} alt= "verizon"/></div>
         </div>
     </section>
     <section className= 'technologies'> 
         <h1>Technologies</h1>
         <hr/>
+         <div>
+          <div></div>
+        </div>
         <div className = "techList">
         </div>
     </section>
@@ -55,47 +56,47 @@ const IndexPage = () => (
         <div className = "projectsList">
           <div className = "projectItems">
             <img src= {SFTProject} alt= "Solve For Tomorrow"/>
-            <p>Techonology, Technology</p>
-            <h2>Samsung:<br/> Solve For Tomorrow</h2>
+            <p>HTML5, SASS, Javascript</p>
+            <h2>Solve For Tomorrow</h2>
           </div>
           <div className = "projectItems">
             <img src= {FRSProject} alt= "First Responders"/>
-            <p>Techonology, Technology</p>
-            <h2>Verizon:<br/> First Responders</h2>
+            <p>HTML5, SASS, React.js</p>
+            <h2>First Responders</h2>
           </div> 
           <div className = "projectItems">
             <img src= {AboveAllSecurity} alt= "Above All Security"/>
-            <p>Techonology, Technology</p>
+            <p>HTML5, CSS3, React.js, Node.js</p>
             <h2>Above All Security</h2>
           </div>
           <div className = "projectItems">
-          <img src= {ProvideServices} alt= "Provide Services"/>
-            <p>Techonology, Technology</p>
+          <img src= "#" alt= "Provide Services"/>
+            <p>HTML5, CSS3, React.js, Node.js </p>
             <h2>Provide Services</h2>
           </div>
           <div className = "projectItems">
             <img src= {WeatherApp} alt= "My Weather App"/>
-            <p>Techonology, Technology</p>
+            <p>HTML5, CSS3, React.js </p>
             <h2>My Weather</h2>
           </div>
           <div className = "projectItems">
             <img src= {FoodSearches} alt= "Food Searchers"/>
-            <p>Techonology, Technology</p>
+            <p>HTML5, CSS3, React.js, Node.js</p>
             <h2>Food Searches</h2>
           </div>
           <div className = "projectItems">
             <img src= {TriviaGame} alt= "Trivia Game"/>
-            <p>Techonology, Technology</p>
+            <p>THTML5, CSS3, and jQuery </p>
             <h2>Trivia Game</h2>
           </div>
           <div className = "projectItems">
             <img src= {ShoppingList} alt= "Shopping List"/>
-            <p>Techonology, Technology</p>
+            <p>HTML, CSS, Ruby on Rails</p>
             <h2>Shopping List</h2>
           </div>
           <div className = "projectItems">
             <img src= {HomeFinder} alt= "Home Finder"/>
-            <p>Techonology, Technology</p>
+            <p>HTML5, CSS3, React.js, Node.js</p>
             <h2>Home Finder</h2>
           </div>
         </div>
