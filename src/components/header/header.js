d61aecbcf081e70ac7fafad45b193eab40f0a3f1
Angie Spears <angie.spears809@gmail.com>
2019-04-25 11:39:17 -0400

    update mobile  menu style

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,54 +1,56 @@
-import PropTypes from "prop-types"
-import React from "react"
-import { Link } from "gatsby"
-import "gatsby-plugin-sass"
-import Resume from "../resume/AngieSpears_Resume.pdf"
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Link } from 'gatsby'
+import 'gatsby-plugin-sass'
 
-import {
-  FaStream
-} from 'react-icons/fa';
+import Resume from '../resume/AngieSpears_Resume.pdf'
 
-import "./header.scss"
+import { FiExternalLink } from 'react-icons/fi'
+import { TiThMenu, TiTimes } from 'react-icons/ti'
+
+import './header.scss'
 
 // function to access links in mobile version
 const UpdateHeader = () => {
-  let menuItems = document.querySelectorAll('.linksContainer')
-  let menuOpen = document.querySelector('#menuIcon')
-  let menuExit = document.querySelector('#menuExitIcon')
-  if (window.innerWidth <= 600) {
+  const menuItems = document.querySelectorAll('.linksContainer')
+  const menuOpen = document.querySelector('#menuIcon')
+  const menuExit = document.querySelector('#menuExitIcon')
+
+  if (window.innerWidth < 678) {
     for (let i = 0; i < menuItems.length; i++) {
-      menuItems[i].classList.toggle("active");
-      menuItems[i].addEventListener('click', UpdateHeader, false);
+      menuItems[i].classList.toggle('active');
+      menuItems[i].addEventListener('click', UpdateHeader, false)
     }
   } 
-    if (document.querySelector(".active") !== null) {
-      menuOpen.style.display = "none"
-      menuExit.style.display = "block"
-    } else {
-      menuOpen.style.display = "block"
-      menuExit.style.display = "none"
-    }
 
-  
-
-  
+  if (document.querySelector('.active') !== null) {
+    menuOpen.style.display = 'none'
+    menuExit.style.display = 'block'
+  } else {
+    menuOpen.style.display = 'block'
+    menuExit.style.display = 'none'
+  }
 }
-    
+
 const Header = ({ siteTitle, home, aboutMe, resume }) => (
   <header>
       <nav>
         <ul>
-          <div className ="mobileNav">
+          <div className = 'mobileNav'>
             <li><Link to='/' id= 'logo'> {siteTitle}</Link></li>
            <div>
-            <li><button id = "menuIcon" onClick = {UpdateHeader}><FaStream/></button></li>
-            <li><button id = "menuExitIcon" onClick = {UpdateHeader}>X</button></li>
+            <li><button id = 'menuIcon' onClick = {UpdateHeader}><TiThMenu/></button></li>
+            <li><button id = 'menuExitIcon' onClick = {UpdateHeader}><TiTimes/></button></li>
             </div>
           </div>
           <div className= 'linksContainer'>
             <li><Link to= '/' className= 'links'>{home}</Link></li>
             <li><Link to= '/about-me' className= 'links'>{aboutMe}</Link></li>
-            <li><a href= {Resume} target= "blank"  className= 'links' id= 'resume'>{resume}</a></li>
+            { window.innerWidth < 768 ? 
+              <li><a href= {Resume} target= 'blank'  className= 'links' id= 'resume'>{resume} <FiExternalLink/></a></li>
+            : 
+              <li><a href= {Resume} target= 'blank'  className= 'links' id= 'resume'>{resume}</a></li>
+            }
           </div>
         </ul>
       </nav>
