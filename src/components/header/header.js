b16a3492694884e0b6e8be5fea8ea1bb26934d52
Angie Spears <angie.spears809@gmail.com>
2019-04-24 21:56:22 -0400

    add mobile header initial exit func

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,20 +4,34 @@ import { Link } from "gatsby"
 import "gatsby-plugin-sass"
 import Resume from "../resume/AngieSpears_Resume.pdf"
 
-import { FaStream } from 'react-icons/fa';
+import {
+  FaStream
+} from 'react-icons/fa';
 
 import "./header.scss"
 
 // function to access links in mobile version
 const UpdateHeader = () => {
   let menuItems = document.querySelectorAll('.linksContainer')
-
+  let menuOpen = document.querySelector('#menuIcon')
+  let menuExit = document.querySelector('#menuExitIcon')
   if (window.innerWidth <= 600) {
     for (let i = 0; i < menuItems.length; i++) {
       menuItems[i].classList.toggle("active");
       menuItems[i].addEventListener('click', UpdateHeader, false);
     }
   } 
+    if (document.querySelector(".active") !== null) {
+      menuOpen.style.display = "none"
+      menuExit.style.display = "block"
+    } else {
+      menuOpen.style.display = "block"
+      menuExit.style.display = "none"
+    }
+
+  
+
+  
 }
     
 const Header = ({ siteTitle, home, aboutMe, resume }) => (
@@ -26,7 +40,10 @@ const Header = ({ siteTitle, home, aboutMe, resume }) => (
         <ul>
           <div className ="mobileNav">
             <li><Link to='/' id= 'logo'> {siteTitle}</Link></li>
+           <div>
             <li><button id = "menuIcon" onClick = {UpdateHeader}><FaStream/></button></li>
+            <li><button id = "menuExitIcon">X</button></li>
+            </div>
           </div>
           <div className= 'linksContainer'>
             <li><Link to= '/' className= 'links'>{home}</Link></li>
