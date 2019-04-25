27fa5ce2e6f89544bc035dfa4eb7d1a06695c648
Angie Spears <angie.spears809@gmail.com>
2019-04-25 14:24:51 -0400

    create pages for projects

---
+++ src/pages/frs-verizon.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+import Projects from "../components/projects/projects"
+
+
+const FRSVerizon = () => (
+    <Layout>
+        <SEO title='Provide Services'/>
+         <section className = "projects">
+            <h1>Other projects</h1>
+            <hr/>
+            <Projects/>
+        </section>
+    </Layout>
+)
+
+export default FRSVerizon
