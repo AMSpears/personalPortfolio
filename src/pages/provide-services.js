0d19704f94b13a9814ffb8df3eb8bb160d6ee7bd
Angie Spears <angie.spears809@gmail.com>
2019-04-25 13:51:45 -0400

    add provide services page initial layout

---
+++ src/pages/provide-services.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+//header 
+import HeaderImg from '../images/projects/provideServices/provideHeader.png'
+
+// project overview
+import ProjectImgOne from '../images/projects/provideServices/provideOne.png'
+import ProjectImgTwo from '../images/projects/provideServices/provideTwo.png'
+
+import Projects from "../components/projects/projects"
+
+import '../components/projectsPages/provideServices.scss'
+
+
+const ProvdeServices = () => (
+    <Layout>
+        <SEO title='Provide Services' />
+        <section className = 'imgPlaceholder'>
+            <img id= 'headerImg' src= {HeaderImg} alt= 'header'/>
+        </section>
+        <section className = 'about'> 
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+        </section>
+        <section className = 'projectImgOne'>
+            <img src= {ProjectImgOne} alt= 'Provide Services Overview'/>
+        </section>
+        <section className = 'about'> 
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+        </section>
+         <section className = 'projectImgTwo'>
+            <img src= {ProjectImgTwo} alt= 'Provide Services Overview'/>
+        </section>
+        <Projects/>
+    </Layout>
+)
+
+
+export default ProvdeServices
