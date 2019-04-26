12d9b7349610bd7dc2b77b5e882f37bb01eb58ac
Angie Spears <angie.spears809@gmail.com>
2019-04-26 17:11:36 -0400

    update above all security page img

--- src/pages/above-all-security.js
+++ src/pages/above-all-security.js
@@ -1,25 +1,35 @@
 import React from 'react'
+import { graphql , StaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-//header 
-import HeaderImg from '../images/web-full-width.png'
 
 import Projects from "../components/projects/projects"
 
 
 const AboveAllSecurity = () => (
-    <Layout>
-        <SEO title='Above All Security'/>
-        <section className = 'imgPlaceholder'>
-            <img id= 'headerImg' src= {HeaderImg} alt= 'header'/>
-        </section>
-         <section className = "projects">
-            <h1>Other projects</h1>
-            <hr/>
-            <Projects/>
-        </section>
-    </Layout>
+    <StaticQuery query = { graphql `{
+        HeaderImg: file(relativePath: {eq: "/web-full-width.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 1600 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    `} render={(data) => (
+        <Layout>
+            <SEO title='Above All Security'/>
+            <section className = 'imgPlaceholder'>
+                <Img fluid={data.HeaderImg.childImageSharp.fluid} fadeIn={false} alt= 'header'/>
+            </section>
+            <section className = "projects">
+                <h1>Other projects</h1>
+                <hr/>
+                <Projects/>
+            </section>
+        </Layout>
+    )}/> 
 )
 
 export default AboveAllSecurity
