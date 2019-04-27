16d8595ad3896fe06b223c82b9b3ff4fe99dbd37
Angie Spears <angie.spears809@gmail.com>
2019-04-26 20:41:20 -0400

    refactor technologies images

--- src/components/technologies/technologies.js
+++ src/components/technologies/technologies.js
@@ -1,37 +1,93 @@
 import React from 'react'
 import 'gatsby-plugin-sass'
-
-// Techologies logos 
-import JavaScript from '../../images/logos/javascript.png'
-import MongoDB from '../../images/logos/mongodb.png'
-import Api from '../../images/logos/apiLogo.png'
-import HtmlCss from '../../images/logos/logo_html-css.png'
-import Reactjs from '../../images/logos/logo_react.png'
-import Express from '../../images/logos/express-js.png'
-import Ruby from '../../images/logos/logo_ruby.png'
-import Sass from '../../images/logos/logo_sass.png'
-import Node from '../../images/logos/node-logo.png'
-
+import { graphql , StaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 import './technologies.scss'
 
-
-const Technologies = () => (
-   <section className= 'technologies'> 
+const Technologies= () => (
+    <StaticQuery query = { graphql `{
+        JavaScript: file(relativePath: {eq: "logos/javascript.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 200 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        MongoDB: file(relativePath: {eq: "logos/mongodb.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 1600 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        Api: file(relativePath: {eq: "logos/apiLogo.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 1600 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        HtmlCss: file(relativePath: {eq: "logos/logo_html-css.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 1600 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        Reactjs: file(relativePath: {eq: "logos/logo_react.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 1600 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        Express: file(relativePath: {eq: "logos/express-js.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 1600 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        Ruby: file(relativePath: {eq: "logos/logo_ruby.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 1600 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        Sass: file(relativePath: {eq: "logos/logo_sass.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 1600 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        Node: file(relativePath: {eq: "logos/node-logo.png"}) {
+            childImageSharp {
+                fluid(maxWidth: 1600 ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`} render={(data) => (
+      <section className= 'technologies'> 
         <h1>Technologies</h1>
         <hr/>
         <div className = 'techList'>
-            <div><img id='js' src={JavaScript} alt='Javascript' /></div>
-            <div><img id = 'api' src= {Api} alt= 'RestFulAPIs'/></div>
-            <div><img id = 'reactjs' src= {Reactjs} alt= 'ReactJS'/></div> 
-            <div><img id = 'node' src= {Node} alt= 'NodeJS'/></div>
-            <div><img id = 'express' src= {Express} alt= 'ExpressJS'/></div> 
-            <div><img id= 'mongodb' src={MongoDB} alt= 'MongoDB' /></div>
-            <div><img id = 'htmlCss' src= {HtmlCss} alt= 'HTML5 | CSS3'/></div>
-            <div><img id = 'sass' src= {Sass} alt= 'Sass'/></div>
-            <div><img id = 'ruby' src= {Ruby} alt= 'Ruby'/></div>
+            <div><Img fluid={data.JavaScript.childImageSharp.fluid} fadeIn={false} alt= 'JavaScript'/></div>
+            <div><Img fluid={data.Api.childImageSharp.fluid} fadeIn={false} alt= 'RestFulAPIs'/></div>
+            <div><Img fluid={data.Reactjs.childImageSharp.fluid} fadeIn={false} alt= 'ReactJS'/></div>
+            <div><Img fluid={data.Node.childImageSharp.fluid} fadeIn={false} alt= 'NodeJS'/></div>
+            <div><Img fluid={data.Express.childImageSharp.fluid} fadeIn={false} alt= 'ExpressJS'/></div>
+            <div><Img fluid={data.MongoDB.childImageSharp.fluid} fadeIn={false} alt= 'MongoDB'/></div>
+            <div><Img fluid={data.HtmlCss.childImageSharp.fluid} fadeIn={false} alt= 'HtmlCss'/></div>
+            <div><Img fluid={data.Sass.childImageSharp.fluid} fadeIn={false} alt= 'Sass'/></div>
+            <div><Img fluid={data.Ruby.childImageSharp.fluid} fadeIn={false} alt= 'Ruby'/></div>
         </div>
     </section>
-)
+)}/>)
+
 
 
 export default Technologies
