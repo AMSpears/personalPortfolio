a4622eda5b9c5d44f67c5f5c096776ad080ea50b
Angie Spears <angie.spears809@gmail.com>
2019-05-04 09:00:12 -0400

    fix technologies display

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -4,7 +4,6 @@ import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { GoPrimitiveDot } from "react-icons/go";
 
 import '../components/about.scss'
 
@@ -34,28 +33,8 @@ const AboutPage = () => (
             <p>Thanks for stopping by !</p>
             <p> My name is Angie Spears. I'm a Front End Software Engineer currently based in NYC.  I enjoy all things tech, chocolate, and coffee.  Bringing quality designs to life is what I'm here to do. I believe in strategizing, planning and executing, ( Yup! ) all in that order.  I'm a General Assembly grad with an undergrad in Business.  If I'm not building things, you will find me traveling or discovering new restaurants in the city.</p>
             <h2>Technologies</h2>
-            <p>
-              JavaScript<GoPrimitiveDot className = "dot"/> 
-              ReactJs<GoPrimitiveDot className = "dot"/> 
-              PreactJs<GoPrimitiveDot className = "dot"/>  
-              GatsbyJs<GoPrimitiveDot className = "dot"/> 
-              NodeJs<GoPrimitiveDot className = "dot"/> 
-              MongoDB<GoPrimitiveDot className = "dot"/> 
-              PSQL / SQL<GoPrimitiveDot className = "dot"/>  
-              ExpressJs<GoPrimitiveDot className = "dot"/>  
-              PugJs<GoPrimitiveDot className = "dot"/>  
-              HTML5<GoPrimitiveDot className = "dot"/> 
-              CSS3<GoPrimitiveDot className = "dot"/> 
-              SASS<GoPrimitiveDot className = "dot"/> 
-              jQuery<GoPrimitiveDot className = "dot"/>  
-              JSON<GoPrimitiveDot className = "dot"/>  
-              Contentful<GoPrimitiveDot className = "dot"/>  
-              Media Queries<GoPrimitiveDot className = "dot"/>  
-              Git<GoPrimitiveDot className = "dot"/> 
-              GitHub<GoPrimitiveDot className = "dot"/> 
-              RESTful APIs<GoPrimitiveDot className = "dot"/> 
-              Ruby<GoPrimitiveDot className = "dot"/>  
-              Rails 
+            <p> 
+              JavaScript <span>&#8226;</span> ReactJs <span>&#8226;</span> PreactJs <span>&#8226;</span> GatsbyJs <span>&#8226;</span> NodeJs <span>&#8226;</span> MongoDB <span>&#8226;</span> PSQL / SQL <span>&#8226;</span> ExpressJs <span>&#8226;</span> PugJs <span>&#8226;</span> HTML5 <span>&#8226;</span> CSS3 <span>&#8226;</span> SASS <span>&#8226;</span> jQuery <span>&#8226;</span> JSON <span>&#8226;</span> Contentful <span>&#8226;</span> Media Queries <span>&#8226;</span> Git <span>&#8226;</span> GitHub <span>&#8226;</span> RESTful APIs <span>&#8226;</span> Ruby <span>&#8226;</span> Rails 
             </p>
           </div>
         </div>
